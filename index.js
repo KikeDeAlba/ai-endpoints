e207de9755104621970eab8ed5d590af8ea6b7c6
KikeDeAlba <likingvirus1@gmail.com>
2024-05-12 22:22:14 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { serve } from '@hono/node-server'
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    baseURL: 'http://localhost:11434/v1',
+    apiKey: 'ollama', // required but unused
+  })
+
+
+const app = new Hono();
+
+app.get('/8ball', async (c) => {
+    const url = new URL(c.req.url)
+    
+    const { question } = Object.fromEntries(url.searchParams)
+
+    console.log(question)
+
+    const completion = await openai.chat.completions.create({
+        model: '8ball',
+        messages: [{ role: 'user', content: question }],
+      })
+
+    return c.text(completion.choices[0].message.content)
+})
+
+serve(app);
